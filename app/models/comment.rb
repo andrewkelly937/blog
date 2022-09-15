857eb08a8ead0decc73aa4920c2e8b31d92dd0dd
Andrew Kelly <andrew.kelly937@gmail.com>
2022-09-15 17:03:46 -0400

    Associations between posts, comments, and users

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,7 +7,9 @@
 #  body       :text
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :integer
 #
 class Comment < ApplicationRecord
   belongs_to :post
+  belongs_to :user
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,9 @@
 #  body       :text
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :integer
 #
 class Post < ApplicationRecord
+  has_many :comments, dependent: :destroy
+  belongs_to :user
 end
